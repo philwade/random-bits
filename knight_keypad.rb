8ad86a6fb7d130ff290ac994d7da01125ae09473
Phil Wade <phil@philwade.org>
2012-01-26 13:01:43 -0500

    Ruby/Dynamic programming version of the knights keypad

---
+++ knight_keypad.rb
@@ -0,0 +1,36 @@
+#this is a ruby version of the knights keypad problem, solved using the dynamic programming solution seen
+# on stackoverflow here: http://stackoverflow.com/questions/2893470/generate-10-digit-number-using-a-phone-keypad
+# thanks for reading!
+
+paths = [
+        [4, 6], #0
+        [6,8], #1
+        [7,9], #2
+        [4,8], #3
+        [0, 3, 9], #4
+        [], #5
+        [7, 1, 0], #6
+        [6,2], #7
+        [3,1], #8
+        [4,2] #9
+        ]
+
+n = ARGV[0].to_i
+
+counts = Array.new(n + 1) { Array.new(10) { |i| i } }
+
+(0..9).each do |i|
+    counts[1][i] = 1
+end
+
+(2..n).each do |n2|
+    (0..9).each do |i2|
+        sum = 0
+        paths[i2].each do |from|
+            sum += counts[n2 - 1][from]
+        end
+        counts[n2][i2] = sum
+    end
+end
+
+puts counts[n][1]
