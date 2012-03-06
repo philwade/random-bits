ce9622ee02b201629cf5e3a3576feaeb43090463
Phil Wade <phil@philwade.org>
2012-03-06 17:12:12 -0500

    Nope. Found a broken sort... checking in to fix later

--- quickSort.py
+++ quickSort.py
@@ -32,7 +32,7 @@ class QuickSorter:
             self.inPlaceQuickSort(set, mid + 1, right)
             
     def partition(self, set, left, right):
-        pivotIndex = (right / 2) + 1
+        pivotIndex = right / 2
         pivotValue = set[pivotIndex]
         print pivotValue
         set[pivotIndex] = set[right]
@@ -59,6 +59,6 @@ if __name__ == "__main__":
     q = QuickSorter()
     runset = [5, 1, 2, 3, 4]
     #print q.quickSort(runset)
-    runset = [1, 5, 7, 3, 9]
+    runset = [5, 7, 2, 3, 4]
     q.inPlaceQuickSort(runset, 0, len(runset) - 1)
     print runset
